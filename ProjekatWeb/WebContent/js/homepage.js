0804aa97dbde394c88974a030e03f43265b4e7eb
Djordje Stevanovic <djordje97.tkd@gmail.com>
2018-01-18 00:13:09 +0100

    Created homepage

--- ProjekatWeb/WebContent/js/homepage.js
+++ ProjekatWeb/WebContent/js/homepage.js
@@ -17,4 +17,42 @@ $(document).ready(function() {
 			status=false;
 		}
 	});
+	var userNameInput = $('#userNameInput');
+	var passwordInput = $('#passwordInput');
+	var messageParagraph = $('#message');
+	$('#logInSubmit').on('click', function(event) { // izvršava se na klik na dugme
+		var userName = userNameInput.val();
+		var password = passwordInput.val();
+		if(userName=="" || password =="")
+			messageParagraph.text("You must enter your username and password");
+		userNameInput.on('focus',function(){
+			messageParagraph.text("");
+			event.preventDefault();
+			return false;
+		});
+		passwordInput.on('focus',function(){
+			messageParagraph.text("");
+			event.preventDefault();
+			return false;
+		});
+		console.log('userName: ' + userName);
+		console.log('password: ' + password);
+
+		// kontrola toka se račva na 2 grane
+		$.post('Login', {'userName': userName, 'password': password}, function(data) { // u posebnoj programskoj niti se šalje (asinhroni) zahtev
+			// tek kada stigne odgovor izvršiće se ova anonimna funkcija
+			console.log('stigao odgovor');
+			console.log(data);
+
+			if (data.status == 'success') {
+				window.location.replace('User.html');
+			}
+		});
+		// program se odmah nastavlja dalje, pre nego što stigne odgovor
+		console.log('poslat zahtev');
+
+		// zabraniti da browser obavi podrazumevanu akciju pri događaju
+		event.preventDefault();
+		return false;
+	});
 });
---
+++ ProjekatWeb/src/projekatWeb/servlets/Login.java
@@ -0,0 +1,63 @@
+package projekatWeb.servlets;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+/**
+ * Servlet implementation class Login
+ */
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String userName = request.getParameter("userName");
+		String password = request.getParameter("password");
+
+		String status = "success";
+		if(userName.toUpperCase().equals("BB") && password.toUpperCase().equals("BB"))
+			status="success";
+		else {
+			status="faliuer";
+		}
+		Map<String, Object> data = new HashMap<>();
+		data.put("status", status);
+
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonData = mapper.writeValueAsString(data);
+		System.out.println(jsonData);
+
+		response.setContentType("application/json");
+		response.getWriter().write(jsonData);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
