251e496411bf72a1788f7be6b73cecdc65b0c12e
Djordje Stevanovic <djordje97.tkd@gmail.com>
2018-02-01 22:00:22 +0100

    Created user page

---
+++ ProjekatWeb/WebContent/js/user.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+
+function openNav() {
+    document.getElementById("user-menu").style.width = "230px";
+}
+function closeNav() {
+    document.getElementById("user-menu").style.width = "0";
+}
